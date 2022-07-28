3d6aa68e7f748e8043c4ff145d111b763b79e6a8
maximiliano <maxil5gomez@gmail.com>
2022-07-27 21:32:33 -0300

    Agregado formulario y muestra de gastos

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Modal from './Modal'
+import ListadoGastos from './components/ListadoGastos'
+import { generarId } from './helpers'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
+
+function App() {
+
+  const [presupuesto, setPresupuesto] = useState(0)
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+  const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
+  const [gastos, setGastos] = useState([])
+
+  const handleNuevoGasto = () => {
+    setModal(true)
+
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 400);
+  }
+
+  const guardarGasto = gasto => {
+    gasto.id = generarId();
+    gasto.fecha = Date.now();
+    setGastos([...gastos, gasto])
+
+    setAnimarModal(false)
+    setTimeout(() => {
+        setModal(false)
+    }, 400);
+  }
+
+  return (
+    <div>
+      <Header 
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+
+      {isValidPresupuesto && (
+        <>
+        <main>
+          <ListadoGastos
+            gastos={gastos}
+          />
+        </main>
+        <div className="nuevo-gasto">
+          <img src={IconoNuevoGasto} alt="Icono nuevo gasto" onClick={handleNuevoGasto} />
+        </div>
+        </>
+      )}
+
+      {modal && <Modal 
+                  setModal={setModal} 
+                  animarModal={animarModal} 
+                  setAnimarModal={setAnimarModal}
+                  guardarGasto={guardarGasto}
+                />}
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useState } from 'react';
+import Mensaje from './Mensaje';
+
+const NuevoPresupuesto = ({
+  presupuesto, 
+  setPresupuesto, 
+  setIsValidPresupuesto
+}) => {
+
+  const [mensaje, setMensaje] = useState('')
+
+  const handlePresupuesto = e => {
+    e.preventDefault();
+    if(!presupuesto || presupuesto < 0){
+      setMensaje('No es un presupuesto valido')
+
+      return
+    } 
+
+    setMensaje('')
+    setIsValidPresupuesto(true)
+    console.log(presupuesto)
+  }
+
+  return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+        <form onSubmit={handlePresupuesto} className='formulario' >
+            <div className="campo">
+                <label>Definir Presupuesto</label>
+                <input type="number" 
+                        className='nuevo-presupuesto'
+                        placeholder='Agrega tu presupuesto'
+                        value={presupuesto}
+                        onChange = { e => setPresupuesto(Number(e.target.value))}
+                
+                />
+            </div>
+            <input type="submit" value='Agregar'/>
+            {mensaje && <Mensaje tipo='error' >{mensaje}</Mensaje>}
+        </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
